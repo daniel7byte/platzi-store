5a424209fec3087544e136ac0166cb9d28d26dd8
Jose Daniel Posso Garcia <daniel7byte@gmail.com>
2021-05-05 17:10:45 -0500

    todo muy pro

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TerminusModule } from '@nestjs/terminus';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsController } from './controllers/products.controller';
+import { HealthController } from './health/health.controller';
+import { BrandsModule } from './brands/brands.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, ProductsController],
+  imports: [TerminusModule, BrandsModule],
+  controllers: [AppController, ProductsController, HealthController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -2,12 +2,7 @@ import { Controller, Get, Param, Query } from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
-  @Get('product/:productId')
-  getProduct(@Param() params: any) {
-    return `product ${params.productId}`
-  }
-
-  @Get('products')
+  @Get()
   getProducts(
     @Query('limit') limit: number = 100,
     @Query('offset') offset: number = 0
@@ -15,8 +10,16 @@ export class ProductsController {
     return `products: limit ${limit}, offset ${offset}`
   }
 
+  @Get(':productId')
+  getProduct(@Param() params: any) {
+    return `product ${params.productId}`
+  }
+
   @Get('branch-office/:branchOfficeId/category/:categoryId')
-  getProductsByBranchOfficeIdAndCategoryId(@Param('branchOfficeId') branchOfficeId: string, @Param('categoryId') categoryId: string) {
+  getProductsByBranchOfficeIdAndCategoryId(
+    @Param('branchOfficeId') branchOfficeId: string,
+    @Param('categoryId') categoryId: string
+  ) {
     return `getProductsByBranchOfficeIdAndCategoryId ${branchOfficeId} - ${categoryId}`;
   }
 }
